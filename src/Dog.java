e72cbbd275014f6bed9c7bedcffaa34d17bc481f
Bektur <Bekturhanazarov98@gmail.com>
2023-12-04 15:00:57 +0600

    dog

---
+++ src/Dog.java
@@ -0,0 +1,12 @@
+public class Dog {
+    String name;
+    int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Dog() {
+    }
+}
